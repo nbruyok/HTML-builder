1a60dbb4b9b0e07179397161f38526fd55a05210
Nastya Bruyok <nastyabruyok@gmail.com>
2022-11-08 01:48:17 +0300

    feat: add solution for task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,45 @@
+const fs = require('fs');
+const path = require('path');
 
+const folderPath = path.join(__dirname, 'files');
+const destPath = path.join(__dirname, 'files-copy');
+
+function copyFolder() {
+  fs.mkdir(destPath, { recursive: true }, (error) => {
+    if (error) {
+      console.error(error);
+    }
+
+    fs.readdir(destPath, (error, files) => {
+      if (error) {
+        console.error(error);
+      }
+      files.forEach((file) => {
+        fs.unlink(path.join(destPath, file), (error) => {
+          if (error) {
+            console.error(error);
+          }
+        });
+      });
+    });
+
+    fs.readdir(folderPath, { withFileTypes: true }, (error, files) => {
+      if (error) {
+        console.error(error);
+      }
+      files.forEach((file) => {
+        fs.copyFile(
+          path.join(folderPath, file.name),
+          path.join(destPath, file.name),
+          (error) => {
+            if (error) {
+              console.error(error);
+            }
+          }
+        );
+      });
+    });
+  });
+}
+
+copyFolder();
